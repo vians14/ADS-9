054489f4940ea273a2728ecb5bca9d792ef57a0a
vians14 <131287803+vians14@users.noreply.github.com>
2026-06-07 19:46:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,3 +5,14 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
+    return tree.getAllPerms();
+}
+
+std::vector<char> getPerm1(PMTree& tree, int num) {
+    return tree.getPermByNumberSlow(num);
+}
+
+std::vector<char> getPerm2(PMTree& tree, int num) {
+    return tree.getPermByNumberFast(num);
+}
